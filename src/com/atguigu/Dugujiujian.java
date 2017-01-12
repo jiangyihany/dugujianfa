5d097ab9baa5cb031a919ae30bff6567329d96e0
岳不群 <ajiangax@163.com>
2017-01-12 16:21:17 +0800

    第二次

--- src/com/atguigu/Dugujiujian.java
+++ src/com/atguigu/Dugujiujian.java
@@ -3,5 +3,10 @@ package com.atguigu;
 public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("只会吹牛逼");
+		System.out.println("只会吹牛逼");
+		System.out.println("只会吹牛逼");
+		System.out.println("只会吹牛逼");
+		System.out.println("只会吹牛逼");
+		
 	}
 }
